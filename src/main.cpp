4c09726bcb991684f63dd0aae73589f82b51db3e
Usama Alshughry <mrsamsh@msn.com>
2026-06-18 17:43:21 +0300

    now faster on mac

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,7 @@ struct GContext
   array_t cells2;
   array_t* current_cells = &cells1;
   array_t* next_cells = &cells2;
+  array_t pixels;
   void swap_cells() {
     array_t* temp = current_cells;
     current_cells = next_cells;
@@ -142,37 +143,18 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
     .entrypoint = "FSmain",
     .format = format,
     .stage = SDL_GPU_SHADERSTAGE_FRAGMENT,
+    .num_storage_buffers = 1,
   };
 
   SDL_GPUShader* fragment_shader = SDL_CreateGPUShader(context.device, &fshader_info);
 
-  SDL_GPUVertexBufferDescription vb_desc[] = {
-    {
-      .slot = 0,
-      .pitch = sizeof(GContext::array_t::type_t),
-      .input_rate = SDL_GPU_VERTEXINPUTRATE_INSTANCE,
-    },
-    {
-      .slot = 1,
-      .pitch = sizeof(math::vec2),
-      .input_rate = SDL_GPU_VERTEXINPUTRATE_INSTANCE,
-    }
-  };
-
-  SDL_GPUVertexAttribute v_attrib[] = {
-    {
-      .location = 0,
-      .buffer_slot = 0,
-      .format = SDL_GPU_VERTEXELEMENTFORMAT_INT,
-      .offset = 0
-    },
-    {
-      .location = 1,
-      .buffer_slot = 1,
-      .format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT2,
-      .offset = 0
-    }
-  };
+  // SDL_GPUVertexBufferDescription vb_desc[] = {
+  //   {
+  //     .slot = 0,
+  //     .pitch = sizeof(GContext::array_t::type_t),
+  //     .input_rate = SDL_GPU_VERTEXINPUTRATE_INSTANCE,
+  //   },
+  // };
 
   SDL_GPUColorTargetDescription color_target_desc = {
     .format = SDL_GetGPUSwapchainTextureFormat(context.device, context.window),
@@ -182,10 +164,6 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
     .vertex_shader = vertex_shader,
     .fragment_shader = fragment_shader,
     .vertex_input_state = {
-      .vertex_buffer_descriptions = vb_desc,
-      .num_vertex_buffers = ARRAY_COUNT(vb_desc),
-      .vertex_attributes = v_attrib,
-      .num_vertex_attributes = ARRAY_COUNT(v_attrib),
     },
     .primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
     .target_info = {
@@ -208,7 +186,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
       transformations[x + y * GContext::gridWidth] = {x, y};
 
   SDL_GPUBufferCreateInfo buffer_create_info{
-    .usage = SDL_GPU_BUFFERUSAGE_VERTEX,
+    .usage = SDL_GPU_BUFFERUSAGE_GRAPHICS_STORAGE_READ,
     .size = GContext::array_t::ByteCapacity(),
   };
 
@@ -344,7 +322,7 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     }
 
 
-    i8* pixels = reinterpret_cast<i8*>(SDL_MapGPUTransferBuffer(context.device, context.cell_transfer_buffer, false));
+    i8* pixels = context.pixels.data();
     for (usz i = 0; i < gridWidth * gridHeight; ++i) {
       auto cc = current_cells[i];
       i8 sc = next_cells[i];
@@ -370,7 +348,6 @@ SDL_AppResult SDL_AppIterate(void* appstate)
         }
       }
     }
-    SDL_UnmapGPUTransferBuffer(context.device, context.cell_transfer_buffer);
   };
 
   static bool const* keyboard = SDL_GetKeyboardState(nullptr);
@@ -433,88 +410,92 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     updateCamera(context);
   }
 
-  {
   // update here
 
   // draw here
   // first upload pass
-    std::thread th_draw([&context](){
-      SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(context.device);
-      SDL_GPUCopyPass* copy_pass = SDL_BeginGPUCopyPass(command_buffer);
+  
+  void* map = SDL_MapGPUTransferBuffer(context.device, context.cell_transfer_buffer, false);
+  memcpy(map, context.pixels.data(), context.pixels.ByteCapacity());
+  SDL_UnmapGPUTransferBuffer(context.device, context.cell_transfer_buffer);
 
-      static SDL_GPUTransferBufferLocation location{
-        .transfer_buffer = context.cell_transfer_buffer,
-        .offset = 0
-      };
-      static SDL_GPUBufferRegion region{
-        .buffer = context.cell_buffer,
-        .offset = 0,
-        .size = GContext::array_t::ByteCapacity()
-      };
+  SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(context.device);
+  SDL_GPUCopyPass* copy_pass = SDL_BeginGPUCopyPass(command_buffer);
 
-      SDL_UploadToGPUBuffer(
-          copy_pass,
-          &location,
-          &region,
-          false
-          );
+  static SDL_GPUTransferBufferLocation location{
+    .transfer_buffer = context.cell_transfer_buffer,
+    .offset = 0
+  };
 
-      SDL_EndGPUCopyPass(copy_pass);
+  static SDL_GPUBufferRegion region{
+    .buffer = context.cell_buffer,
+    .offset = 0,
+    .size = GContext::array_t::ByteCapacity()
+  };
 
-      SDL_GPUTexture* swapchain_texture;
-      u32 width, height;
-      SDL_WaitAndAcquireGPUSwapchainTexture(
-          command_buffer,
-          context.window,
-          &swapchain_texture,
-          &width,
-          &height
-          );
-      SDL_GPUColorTargetInfo color_target_info{
-        .texture = swapchain_texture,
-        .clear_color = {0.f, 0.025f, 0.2f, 1.f},
-        .load_op = SDL_GPU_LOADOP_CLEAR,
-        .store_op = SDL_GPU_STOREOP_STORE,
-      };
-      SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(
-          command_buffer,
-          &color_target_info,
-          1,
-          nullptr
-          );
+  SDL_UploadToGPUBuffer(
+      copy_pass,
+      &location,
+      &region,
+      false
+      );
 
-      SDL_BindGPUGraphicsPipeline(render_pass, context.pipeline);
-      SDL_GPUBufferBinding buffer_bind[]{
-        {.buffer = context.cell_buffer},
-      };
-      SDL_BindGPUVertexBuffers(render_pass, 0, buffer_bind, 1);
+  SDL_EndGPUCopyPass(copy_pass);
 
-      SDL_SetGPUViewport(render_pass, &context.viewport);
+  SDL_GPUTexture* swapchain_texture;
+  u32 width, height;
+  SDL_WaitAndAcquireGPUSwapchainTexture(
+      command_buffer,
+      context.window,
+      &swapchain_texture,
+      &width,
+      &height
+      );
 
-      struct {
-        math::mat4 projection;
-        math::vec2 windowSize;
-        math::vec2 gridSize;
-        f32        cellSide;
-      } ubo = {context.matrices.projection * context.matrices.view, {GContext::WindowWidth, GContext::WindowHeight}, {GContext::gridWidth , GContext::gridHeight}, GContext::CellSide};
+  SDL_GPUColorTargetInfo color_target_info{
+    .texture = swapchain_texture,
+    .clear_color = {0.f, 0.025f, 0.2f, 1.f},
+    .load_op = SDL_GPU_LOADOP_CLEAR,
+    .store_op = SDL_GPU_STOREOP_STORE,
+  };
 
-      SDL_PushGPUVertexUniformData(command_buffer, 0, &ubo, sizeof(ubo));
-      SDL_DrawGPUPrimitives(render_pass, 6, context.current_cells->size(), 0, 0);
-      SDL_EndGPURenderPass(render_pass);
-      SDL_SubmitGPUCommandBuffer(command_buffer);
-    });
+  SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(
+      command_buffer,
+      &color_target_info,
+      1,
+      nullptr
+      );
+
+  SDL_BindGPUGraphicsPipeline(render_pass, context.pipeline);
+
+  SDL_GPUBufferBinding buffer_bind[]{
+    {.buffer = context.cell_buffer},
+  };
+
+  SDL_BindGPUFragmentStorageBuffers(render_pass, 0, &context.cell_buffer, 1);
+
+  SDL_SetGPUViewport(render_pass, &context.viewport);
+
+  struct {
+    math::mat4 projection;
+    math::vec2 windowSize;
+    math::vec2 gridSize;
+    f32        cellSide;
+  } ubo = {context.matrices.projection * context.matrices.view, {GContext::WindowWidth, GContext::WindowHeight}, {GContext::gridWidth , GContext::gridHeight}, GContext::CellSide};
+
+  SDL_PushGPUVertexUniformData(command_buffer, 0, &ubo, sizeof(ubo));
+  SDL_DrawGPUPrimitives(render_pass, 6, 1, 0, 0);
+  SDL_EndGPURenderPass(render_pass);
+  SDL_SubmitGPUCommandBuffer(command_buffer);
 
 
-    u64 start = SDL_GetTicksNS();
-    if (updating)
-    {
-      calculateNext(context.current_cells->data(), context.next_cells->data());
-    }
-    std::println("update elapsed: {} ms", (SDL_GetTicksNS() - start) * 1.e-6);
-
-    th_draw.join();
-
+  u64 start = SDL_GetTicksNS();
+  if (updating)
+  {
+    calculateNext(context.current_cells->data(), context.next_cells->data());
   }
+  std::println("update elapsed: {} ms", (SDL_GetTicksNS() - start) * 1.e-6);
+
 
   if (updating)
     context.swap_cells();
@@ -615,64 +596,34 @@ using namespace metal;
 struct UniformBufferObject
 {
   float4x4 projection;
+  float2   windowSize;
   float2   gridSize;
   float    cellSide;
-};
-
-struct VertexIn
-{
-  float2 translation[[attribute(1)]];
-  int color [[attribute(0)]];
 };
 
 struct VertexOut
 {
   float4 position [[position]];
-  float4 color;
+  float2 texcoord;
+  float2 gridSize;
 };
 
 constant float2 VertexPositions[6] = {
   {0.0, 0.0},
-  {0.8, 0.0},
-  {0.8, 0.8},
-  {0.0, 0.8},
+  {1.0, 0.0},
+  {1.0, 1.0},
+  {0.0, 1.0},
   {0.0, 0.0},
-  {0.8, 0.8}
-};
-constant float4 palette[] = {
-  {0.0, 0.100, 0.800, 1},
-  {0.0, 0.095, 0.760, 1},
-  {0.0, 0.090, 0.720, 1},
-  {0.0, 0.085, 0.680, 1},
-  {0.0, 0.080, 0.640, 1},
-  {0.0, 0.075, 0.600, 1},
-  {0.0, 0.070, 0.560, 1},
-  {0.0, 0.065, 0.520, 1},
-  {0.0, 0.060, 0.480, 1},
-  {0.0, 0.055, 0.440, 1},
-  {0.0, 0.050, 0.400, 1},
-  {0.0, 0.045, 0.360, 1},
-  {0.0, 0.040, 0.320, 1},
-  {0.0, 0.035, 0.280, 1},
-  {0.0, 0.030, 0.240, 1},
-  {0.0, 0.025, 0.200, 1},
-  {0.0, 0.020, 0.160, 1},
-  {0.0, 0.015, 0.120, 1},
-  {0.0, 0.010, 0.080, 1},
-  {0.0, 0.005, 0.040, 1},
-  {0, 0.0125, 0.1, 1},
-  {1, 1, 1, 1}
+  {1.0, 1.0}
 };
 
-vertex VertexOut VSmain(VertexIn in [[stage_in]],
-                        uint vertexID [[vertex_id]],
+vertex VertexOut VSmain(uint vertexID [[vertex_id]],
                         constant UniformBufferObject& ubo [[buffer(0)]])
 {
   VertexOut out;
-  out.color = palette[in.color + 20];
-
-  float2 position = (VertexPositions[vertexID] * ubo.cellSide + in.translation * ubo.cellSide);
-  out.position = ubo.projection * float4(position, 0, 1);
+  out.position = ubo.projection * float4(VertexPositions[vertexID] * ubo.windowSize, 0, 1);
+  out.texcoord = VertexPositions[vertexID] * ubo.gridSize;
+  out.gridSize = ubo.gridSize;
   return out;
 }
 )";
@@ -681,16 +632,47 @@ std::string_view GContext::FRAGMENT_SHADER_MSL = R"(
 #include <metal_stdlib>
 using namespace metal;
 
+constant float4 palette[] = {
+  {0.0, 0.100,  0.800, 1},
+  {0.0, 0.095,  0.760, 1},
+  {0.0, 0.090,  0.720, 1},
+  {0.0, 0.085,  0.680, 1},
+  {0.0, 0.080,  0.640, 1},
+  {0.0, 0.075,  0.600, 1},
+  {0.0, 0.070,  0.560, 1},
+  {0.0, 0.065,  0.520, 1},
+  {0.0, 0.060,  0.480, 1},
+  {0.0, 0.055,  0.440, 1},
+  {0.0, 0.050,  0.400, 1},
+  {0.0, 0.045,  0.360, 1},
+  {0.0, 0.040,  0.320, 1},
+  {0.0, 0.035,  0.280, 1},
+  {0.0, 0.030,  0.240, 1},
+  {0.0, 0.025,  0.200, 1},
+  {0.0, 0.020,  0.160, 1},
+  {0.0, 0.015,  0.120, 1},
+  {0.0, 0.010,  0.080, 1},
+  {0.0, 0.005,  0.040, 1},
+  {0.0, 0.025,  0.2,   1},
+  {1,   1,      1,     1}
+};
+
 struct VertexOut
 {
   float4 position [[position]];
-  float4 color;
+  float2 texcoord;
+  float2 gridSize;
 };
 
-
-fragment float4 FSmain(VertexOut in [[stage_in]])
+fragment float4 FSmain(VertexOut in [[stage_in]],
+                        constant int8_t* indices [[buffer0]])
 {
-  return in.color;
+  float2 local = fract(in.texcoord);
+  float margin = 0.1;
+  if (local.x < margin || local.x > 1.0 - margin || local.y < margin || local.y > 1.0 - margin)
+    return float4(0, 0.0125, 0.1, 1);
+  int i = floor(in.texcoord.x) + floor(in.texcoord.y) * in.gridSize.x;
+  return palette[indices[i] + 20];
 }
 )";
 
