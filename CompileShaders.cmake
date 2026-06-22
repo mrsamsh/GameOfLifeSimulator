6edeefab3b1d6ed218ee1aacaf912109b019c0e3
Usama Alshughry <mrsamsh@msn.com>
2026-06-22 15:37:24 +0300

    added separate file to compile shaders

---
+++ CompileShaders.cmake
@@ -0,0 +1,61 @@
+function(set_shader_type type)
+  if("${type}" STREQUAL "METAL" OR "${type}" STREQUAL "SPIRV" OR "${type}" STREQUAL "DXIL")
+    set(SHADER_TYPE ${type} PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "Unrecognized shader type ${type}. Supported types: METAL | SPIRV | DXIL")
+  endif()
+endfunction()
+
+function(add_metal_shader_library libID libName)
+  set(airFiles)
+  set(libName "${libName}.metallib")
+  set(SHADERS_DIR ${CMAKE_BINARY_DIR}/Shaders/Metal)
+  foreach(file ${ARGN})
+    cmake_path(GET file STEM airFilename)
+    set(airFilename "${airFilename}.air")
+    add_custom_command(
+      OUTPUT ${SHADERS_DIR}/${airFilename}
+      DEPENDS ${CMAKE_SOURCE_DIR}/${file}
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${SHADERS_DIR}
+      COMMAND xcrun -sdk macosx metal -c ${CMAKE_SOURCE_DIR}/${file} -o ${SHADERS_DIR}/${airFilename}
+      COMMENT "Compiling ${airFilename}"
+      VERBATIM
+    )
+    list(APPEND airFiles "${SHADERS_DIR}/${airFilename}")
+  endforeach()
+  add_custom_command(
+    OUTPUT ${SHADERS_DIR}/${libName}
+    DEPENDS "${airFiles}"
+    COMMAND xcrun -sdk macosx metal -o ${SHADERS_DIR}/${libName} ${airFiles}
+    COMMENT "Linking Metal library ${libName}"
+    VERBATIM
+  )
+  set("${libID}" ${SHADERS_DIR}/${libName} PARENT_SCOPE)
+endfunction()
+
+function(add_spv_shader_library libID libName)
+  set(SHADERS_DIR ${CMAKE_BINARY_DIR}/Shaders/SPIRV)
+  set(libName ${SHADERS_DIR}/${libName}.spv)
+  foreach(file ${ARGN})
+    cmake_path(GET file STEM filename)
+    string(LENGTH "${filename}" filenameLength)
+    math(EXPR start_index "${filenameLength} - 4")
+    string(SUBSTRING "${filename}" ${start_index} 4 shaderType)
+    if("${shaderType}" STREQUAL "vert")
+      add_custom_command(
+        OUTPUT ${libName}
+        DEPENDS ${CMAKE_SOURCE_DIR}/${file}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${SHADERS_DIR}
+        COMMAND glslc -DUSING_GLSLC=1 -fshader-stage="${shaderType}" -fentry-point"VSmain" -c ${CMAKE_SOURCE_DIR}/${file} -o ${libName}
+        COMMENT "Compiling ${shaderType} shader"
+        VERBATIM
+      )
+    elseif("${shaderType}" STREQUAL "frag")
+    else()
+      message(FATAL_ERROR "Unknown shader type in ${file}, naming convention: name should end with vert or frag")
+    endif()
+    list(APPEND compiledShaders ${libName})
+  endforeach()
+  message("From here: ${compiledShaders}")
+  set("${libID}" ${compiledShaders} PARENT_SCOPE)
+endfunction()
